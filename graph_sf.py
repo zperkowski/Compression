4d7f87735614326150bd35fe8e769304f24e72a3
zperkowski <ZdzislawPerkowski@outlook.com>
2018-12-22 21:00:16 +0100

    Generating binary tree, visualizing

--- graph_sf.py
+++ graph_sf.py
@@ -1,41 +1,48 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
+g = nx.Graph()
+
 
 def draw_graph(g):
     plt.subplot(111)
-    nx.draw(g, with_labels=True, font_weight='bold')
+    nx.draw_shell(g, with_labels=True, font_weight='bold')
+    labels = nx.get_edge_attributes(g, 'weight')
+    pos = nx.shell_layout(g)
+    nx.draw_networkx_edge_labels(g, pos, edge_labels=labels)
     plt.show()
 
 
-def split_dict(dict_p):
-    keys = list(dict_p.keys())
-    values = list(dict_p.values())
+def build_graph(g, node, dict_p, keys, values):
+    draw_graph(g)
     print(keys)
     print(values)
-    l = len(keys)
     splits = []
-    while len(splits) < l:
-        min_diff = abs(sum(values[:1]) - sum(values[1:]))
-        split_after = keys[0]
-        for i in range(len(keys) - 1):
-            diff = abs(sum(values[:i]) - sum(values[i + 1:]))
-            if diff < min_diff and keys[i] not in splits:
-                min_diff = diff
-                split_after = keys[i]
-        splits.append(split_after)
-        i = keys.index(split_after)
-        keys.pop(i)
-        values.pop(i)
-    return splits
+    min_diff = abs(sum(values[:1]) - sum(values[1:]))
+    split_after = keys[0]
+    for i in range(len(keys) - 1):
+        diff = abs(sum(values[:i]) - sum(values[i + 1:]))
+        if diff < min_diff and keys[i] not in splits:
+            min_diff = diff
+            split_after = keys[i]
+    i = keys.index(split_after)
+    if len(keys) > 2:
+        node_value_l = node + 1
+        g.add_edge(node, node_value_l, weight=0)
+        node_value_p = (node + 1) * -1
+        g.add_edge(node, node_value_p, weight=1)
+        build_graph(g, node_value_l, dict_p, keys[:i + 1], values[:i + 1])
+        build_graph(g, node_value_p, dict_p, keys[i + 1:], values[i + 1:])
+    else:
+        g.add_edge(node, keys[0], weight=0)
+        if len(keys) == 2:
+            g.add_edge(node, keys[1], weight=1)
 
 
 def generate_graph(dict_p):
-    g = nx.Graph()
-    g.add_nodes_from(dict_p.keys())
-    splits = split_dict(dict_p)
-    sub_nodes = [i for i in range(len(splits))]
-    g.add_nodes_from(sub_nodes)
+    keys = list(dict_p.keys())
+    values = list(dict_p.values())
+    build_graph(g, 0, dict_p, keys, values)
     return g
 
 
